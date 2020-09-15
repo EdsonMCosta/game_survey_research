885ce34011e38df887cedd079473686c316703f1
Edson M Costa <edsoncosta.jr89@gmail.com>
2020-09-15 19:24:17 -0300

    Project created

---
+++ backend/src/main/java/com/game_survey_research/game_sr/GameSurveyResearchApplication.java
@@ -0,0 +1,13 @@
+package com.game_survey_research.game_sr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSurveyResearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameSurveyResearchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/game_survey_research/game_sr/GameSurveyResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game_survey_research.game_sr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSurveyResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
